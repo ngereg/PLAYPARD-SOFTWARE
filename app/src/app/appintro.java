b6411ae3e1cba20002bb14dbd6ac1562da967156
ruitong23 <64746287+ruitong23@users.noreply.github.com>
2021-03-21 15:40:58 -0400

    Add files via upload

---
+++ app/src/app/appintro.java
@@ -0,0 +1,18 @@
+package compay;
+
+import java.util.Scanner;
+
+public class appintro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.print("Thank you for using PLAYPARD-SOFTWARE\n");
+		System.out.print("please enter your username:" );
+		String username=input.next();
+		System.out.print("please enter your password:" );
+		String password=input.next();
+		
+	}
+
+}
