55cef8e0b378745931cf928aac62789e1cb5bc94
ngereg <80414932+ngereg@users.noreply.github.com>
2021-04-08 21:11:44 -0400

    Add files via upload

---
+++ task.java
@@ -0,0 +1,69 @@
+
+public class task {
+
+	private int id;
+	private String name;
+	private String description;
+	private String date;
+	private int priority;
+	private int progress;
+	
+	
+	public task (int id, String name, String description, String date, int priority, int progress) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.date = date;
+		this.priority = priority;
+		this.progress = progress;
+	}
+	
+	//Getter Methods
+	public int getID() {
+		return id;
+	}
+	
+	public String getname() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public int getPriority() {
+		return priority;
+	}
+	
+	public int getProgress() {
+		return progress;
+	}
+	
+	//Setter Methods
+	
+	public void setname(String name) {
+		this.name = name;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
+	public void setProgress(int progress) {
+		this.progress = progress;
+	}
+	
+	
+}
