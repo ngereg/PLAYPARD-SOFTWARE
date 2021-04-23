862aebfe8b52749be5e19e3ca5f5a1b7172a85a5
Yueheng0628 <64717236+Yueheng0628@users.noreply.github.com>
2021-04-23 11:08:20 -0700

    Add files via upload

---
+++ FreemioIntegrationTest.java
@@ -0,0 +1,39 @@
+/**
+ * integration test top-down 
+ * @author yueheng han
+ *
+ */
+public class FreemioIntegrationTest {
+	
+	/**
+	 * test group 1
+	 * 1. login
+	 * 2. create multiple task
+	 * 3. sort task by name
+	 */
+	public void testGroup1() {
+		
+	}
+	
+	/**
+	 * test group 2
+	 * 1. login
+	 * 2. create multiple task
+	 * 3. search task
+	 */
+	public void testGroup2() {
+		
+	}
+	
+	/**
+	 * test group 3
+	 * 1. login
+	 * 2. create multiple task
+	 * 3. show all the task
+	 * 4. update task
+	 * 5. show all the task again
+	 */
+	public void testGroup3() {
+		
+	}
+}
