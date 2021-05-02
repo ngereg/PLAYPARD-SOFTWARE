268a127b12fa5182c0b8c6c9c8a9020b32f51718
Yueheng0628 <64717236+Yueheng0628@users.noreply.github.com>
2021-05-02 15:15:31 -0700

    Add files via upload

--- tests/TaskTest.java
+++ tests/TaskTest.java
@@ -191,11 +191,53 @@ public class TaskTest {
 		
 		tempTestFile.delete();
 	}
+	
+	public void testTaskGroup() throws IOException {
+		ArrayList<Task> taskGroup = new ArrayList<Task>();
+		File tempTestFile = new File("TestFile2.txt");
+		String dueDate1 = "4/20/2021";
+		String dueDate2 = "4/23/2021";
+		String dueDate3 = "5/20/2021";
+		Task.createTask("btest", "This is a test Task", dueDate2, 1, 0, tempTestFile);
+		Task.createTask("atest", "This is a test Task", dueDate1, 0, -1, tempTestFile);
+		Task.createTask("ctest", "This is a test Task", dueDate3, 0, 1, tempTestFile);
+	}
+	
 	@Test
-	public void testremnder() {
+	public void testreminder() {
 		Date dueDate = new Date(2021-1900, 4-1, 9);
 		String email= "email@email.com";
 		Task task = new Task(0, "task", "This is a test Task", dueDate, 0, 0);
-		assertTrue(Task.setremnder(email,task));
+		// assertTrue(Task.setreminder(email,task));
+	}
+	
+	@Test
+	public void testUpdate() throws IOException {
+		File tempTestFile = new File("TestFile3.txt");
+		String dueDate1 = "4/20/2021";
+		String dueDate2 = "4/23/2021";
+		String dueDate3 = "5/20/2021";
+		Task.createTask("test1", "This is a test Task", dueDate1, 0, 0, tempTestFile);
+		Task.createTask("test2", "This is a test Task", dueDate2, 0, 0, tempTestFile);
+		Task.createTask("test3", "This is a test Task", dueDate3, 0, 0, tempTestFile);
+		String test1 = "0;test1;This is a test Task;4/20/2021;0;1";
+		String test2 = "1;test2;This is a test Task;4/23/2021;0;0";
+		String test3 = "2;test3;This is a test Task;5/20/2021;0;0";
+		ArrayList<String> correctAns = new ArrayList<String>();
+		correctAns.add(test1);
+		correctAns.add(test2);
+		correctAns.add(test3);
+		Task.updateProgress("test1", tempTestFile, 1);
+		Scanner reader = new Scanner(tempTestFile);
+		int i = 0;
+		while (reader.hasNextLine()) {
+			String temp = reader.nextLine();
+			String[] array = temp.split(";");
+			Task temp1 = Task.makeItTask(array);
+			assertEquals(temp1.toString(), correctAns.get(i));
+			i++;
+		}
+		reader.close();
+		tempTestFile.delete();
 	}
 }
